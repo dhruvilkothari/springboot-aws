cc59dda264380bc407e7e7fa0c57a546b4b4c217
Dhruvil Kothari <kotharidhruvil3@gmail.com>
2026-04-03 16:22:57 +0530

    Added Routes

--- demo/src/main/java/com/example/CrudApp/demo/controller/UserController.java
+++ demo/src/main/java/com/example/CrudApp/demo/controller/UserController.java
@@ -15,12 +15,12 @@ public class UserController {
     private final UserService userService;
 
 
-    @GetMapping("")
+    @GetMapping("/getAllUsers")
     public ResponseEntity<List<UserDto>> getAllUsers(){
         return userService.getAllUsers();
     }
 
-    @PostMapping("")
+    @PostMapping("/create")
     public ResponseEntity<UserDto> createUser(@RequestBody(required = true) UserDto userDto){
         return userService.createUser(userDto);
     }
